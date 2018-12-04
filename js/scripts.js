3f4dfef910e3bdf74425caa3fad2d02fbfc3eed8
Joanna Williams <joannalynwilliams@gmail.com>
2018-12-03 22:58:05 -0500

    Added in a callback function to add markers to places

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,100 @@ var labelIndex = 0;
     var cappadocia = {lat: 38.474839, lng: 35.3678906};
     var bali = {lat: -8.4553718, lng: 114.791387};
 
+    //FourSquare API
+    var clientID = config.CLIENT_ID; //Sets myKey variable = API key
+    var clientSecret = config.CLIENT_SECRET; //Sets myKey variable = API key
+    var latlong = [
+
+      {lat:20.2096165, lng:-87.5068955},//tulum mexico
+      {lat:52.3545653, lng:4.7585408}, //Amsterdam, Netherlands
+      {lat: 37.3505938, lng: 21.6949874}, //Greek Islands
+      {lat: 19.3204434, lng: -99.2926982}, //Mexico City, Mexico
+      {lat: -0.7190938, lng: 72.9293092}, //The Maldives
+      {lat: 37.2450904, lng: -8.4753918}, //Algarve, Portugal
+      {lat: 31.6346023, lng: -8.077893}, //Marrakesh, Morocco
+      {lat: 40.627271, lng: 14.4849303}, //Positano, Italy
+      {lat: 38.474839, lng: 35.3678906}, //Cappadocia, Turkey
+      {lat: -8.4553718, lng: 114.791387} //Bali, Indonesia
+
+  ]
+
+  for (var i = 0; i < latlong.length; i++) {
+    //console.log(latlong[i]);
+    var url2 = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20180323&limit=100&ll='+ latlong[i].lat + ',' + latlong[i].lng + '&query=fun';
+    var data2 = [];
+    var html2 = '';
+    var resp = [];
+    var groups = [];
+    var item = [];
+    var venues = [];
+    var locations = [];
+    var latlongvenues = [];
+
+            var mapCities = new google.maps.Map(
+                    document.getElementById('info-section'), {
+                      zoom:4,
+                      center:tulum
+                    });
+
+            // Loop through the results array and place a marker for each
+    // set of coordinates.
+    window.eqfeed_callback = function(results) {
+      $.ajax({
+        type:'GET',
+        url:url2,
+        dataType: 'json',
+        async: true,
+        data: data2,
+        success: function(data2){
+          //console.log(data2.response);
+          //console.log(data2.response.groups);
+          groups = data2.response.groups;
+          groups.forEach(function(group){
+            //console.log(group);
+            item = group.items;
+            //console.log(item);
+            item.forEach(function(places){
+            //  console.log(places);
+              venues = places.venue;
+            //  console.log(venues);
+              locations = places.venue.location;
+              //console.log(locations);
+              var latvenues = locations.lat;
+              var lngvenues = locations.lng;
+              //console.log(latvenues);
+              //console.log(lngvenues);
+              var pushItems = {'coordinates': [latvenues, lngvenues]};
+              latlongvenues.push(pushItems);
+              // console.log(pushItems);
+              for (var j = 0; j < results.pushItems.length; j++) {
+                var coords = results.pushItems[j].coordinates;
+                console.log(coords);
+                var latLng = new google.maps.LatLng(coords[1],coords[0]);
+                var marker11 = new google.maps.Marker({
+                  position: latLng,
+                  map: mapCities
+                });
+              }//for loop
+
+                        });
+                      });
+
+
+                      } //success
+
+                  }); //closes ajax request
+
+    }
+
+
+
+
+
+  }
+
+
+
     // The map, centered at
 
     var map = new google.maps.Map(
@@ -25,20 +119,12 @@ var labelIndex = 0;
           center:positano
         });
 
-    var mapCities = new google.maps.Map(
-            document.getElementById('info-section'), {
-              zoom:9,
-              center:tulum
-            });
-
-
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({
       position: tulum,
       map: map,
       animation: google.maps.Animation.DROP,
       label: labels[labelIndex++ % labels.length],
-      //icon: './img/tarheel.png' GIANT MARKER
 
     });
 
@@ -163,64 +249,3 @@ $.ajax({
     //  $('marker1').onclick = function(){
             //    $('#city1').addClass('hidden');
           //  });
-
-          //FourSquare API
-          var clientID = config.CLIENT_ID; //Sets myKey variable = API key
-          var clientSecret = config.CLIENT_SECRET; //Sets myKey variable = API key
-          var latlong = [
-
-            {lat:20.2096165, lng:-87.5068955},//tulum mexico
-            {lat:52.3545653, lng:4.7585408}, //Amsterdam, Netherlands
-            {lat: 37.3505938, lng: 21.6949874}, //Greek Islands
-            {lat: 19.3204434, lng: -99.2926982}, //Mexico City, Mexico
-            {lat: -0.7190938, lng: 72.9293092}, //The Maldives
-            {lat: 37.2450904, lng: -8.4753918}, //Algarve, Portugal
-            {lat: 31.6346023, lng: -8.077893}, //Marrakesh, Morocco
-            {lat: 40.627271, lng: 14.4849303}, //Positano, Italy
-            {lat: 38.474839, lng: 35.3678906}, //Cappadocia, Turkey
-            {lat: -8.4553718, lng: 114.791387} //Bali, Indonesia
-
-        ]
-
-        for (var i = 0; i < latlong.length; i++) {
-          console.log(latlong[i]);
-          var url2 = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20180323&limit=100&ll='+ latlong[i].lat + ',' + latlong[i].lng + '&query=hotels';
-          var data2 = [];
-          var html2 = '';
-          var photos = [];
-          var resp = [];
-          var groups = [];
-          var item = [];
-          var venues = [];
-          var locations = [];
-          //var url2 = 'https://api.foursquare.com/v2/venues/explore?ll=' + latlong[i].lat + ',' + latlong[i].lng + '&limit=30&client_id=' + clientID + '&client_secret=' + clientSecret ;
-        }
-          $.ajax({
-            type:'GET',
-            url:url2,
-            dataType: 'json',
-            async: true,
-            data: data2,
-            success: function(data2){
-              console.log(data2.response);
-              console.log(data2.response.groups);
-              groups = data2.response.groups;
-              groups.forEach(function(group){
-                console.log(group);
-                item = group.items;
-                console.log(item);
-                item.forEach(function(places){
-                  console.log(places);
-                  venues = places.venue;
-                  console.log(venues);
-                  locations = places.venue.location;
-                  console.log(locations);
-                });
-              });
-
-
-
-
-              } //success
-
-          }); //closes ajax request
